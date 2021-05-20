2aa53f152e71fa572c67fa0324b24033078b1dee
Pedro Leinar <pedroleinar@hotmail.com>
2021-05-20 20:31:33 -0300

    feat(home): category selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { Header } from './components/Header';
+import { Home } from './pages/Home';
 import { theme } from './theme';
+
 function MyApp() {
   return (
     <ChakraProvider theme={theme}>
-      <Header />
+      <Home />
     </ChakraProvider>
   );
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,86 @@
+import {
+  Menu,
+  MenuList,
+  MenuItem,
+  MenuButton,
+  Stack,
+  Flex,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
+
+import { useEffect, useState } from 'react';
+import { Header } from '../../components/Header';
+import { api } from '../../services/api';
+
+import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons';
+
+type Categories = {
+  id: number;
+  name: string;
+};
+
+export const Home = () => {
+  const [categories, setCategories] = useState<Categories[]>([]);
+  const [selectedCategory, setSelectedCategory] =
+    useState<Categories | undefined>();
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  useEffect(() => {
+    async function getCategorias() {
+      const response = await api.get('/categories');
+      setCategories(response.data);
+    }
+    getCategorias();
+  }, []);
+
+  const handleCategorySelection = (id: number) => {
+    setSelectedCategory(categories.find((c) => c.id === id));
+  };
+
+  const TriangleIcon = isOpen ? TriangleUpIcon : TriangleDownIcon;
+
+  return (
+    <>
+      <Header />
+      <Stack m="16" alignItems="center">
+        <Menu onClose={onClose} onOpen={onOpen}>
+          <MenuButton
+            px={4}
+            py={2}
+            w={600}
+            transition="all 0.2s"
+            borderRadius="md"
+            borderWidth="1px"
+            _hover={{ bg: 'gray.600' }}
+            _expanded={{ bg: 'gray.600' }}
+            _focus={{ boxShadow: 'outline' }}
+          >
+            <Flex alignItems="center" justifyContent="space-between">
+              <Text>
+                {selectedCategory
+                  ? selectedCategory.name
+                  : 'Selecione a categoria'}
+              </Text>
+
+              <TriangleIcon />
+            </Flex>
+          </MenuButton>
+          <MenuList w={600}>
+            {categories.length > 0 &&
+              categories.map((category) => (
+                <MenuItem
+                  key={category.id}
+                  value={category.id}
+                  onClick={() => handleCategorySelection(category.id)}
+                >
+                  {category.name}
+                </MenuItem>
+              ))}
+          </MenuList>
+        </Menu>
+      </Stack>
+    </>
+  );
+};
