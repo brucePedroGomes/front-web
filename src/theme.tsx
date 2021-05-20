9f605c59ec15898ff2399a9fae81a0da0a06d133
Pedro Leinar <pedroleinar@hotmail.com>
2021-05-20 13:58:05 -0300

    chore: add chakra-ui

---
+++ src/theme.tsx
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto',
+  },
+  config: {
+    initialColorMode: 'dark',
+  },
+});
